e242b2f8d27d88cdb00823b0debd225cd768626f
Alexander <38591972+genryxy@users.noreply.github.com>
2021-03-17 17:43:09 +0300

    Add skeleton for proxy implementation
    For #77 - Add skeleton for proxy implementation
    PR: #94

---
+++ src/main/java/com/artipie/composer/http/ComposerProxySlice.java
@@ -0,0 +1,111 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 artipie.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.composer.http;
+
+import com.artipie.asto.cache.Cache;
+import com.artipie.http.Slice;
+import com.artipie.http.client.ClientSlices;
+import com.artipie.http.client.UriClientSlice;
+import com.artipie.http.client.auth.AuthClientSlice;
+import com.artipie.http.client.auth.Authenticator;
+import com.artipie.http.rq.RqMethod;
+import com.artipie.http.rs.RsStatus;
+import com.artipie.http.rs.RsWithStatus;
+import com.artipie.http.rt.ByMethodsRule;
+import com.artipie.http.rt.RtRule;
+import com.artipie.http.rt.RtRulePath;
+import com.artipie.http.rt.SliceRoute;
+import com.artipie.http.slice.SliceSimple;
+import java.net.URI;
+
+/**
+ * Composer proxy repository slice.
+ * @since 0.4
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ */
+public class ComposerProxySlice extends Slice.Wrap {
+    /**
+     * New Composer proxy without cache and without authentication.
+     * @param clients HTTP clients
+     * @param remote Remote URI
+     */
+    public ComposerProxySlice(final ClientSlices clients, final URI remote) {
+        this(clients, remote, Authenticator.ANONYMOUS, Cache.NOP);
+    }
+
+    /**
+     * New Composer proxy without cache.
+     * @param clients HTTP clients
+     * @param remote Remote URI
+     * @param auth Authenticator
+     */
+    public ComposerProxySlice(
+        final ClientSlices clients, final URI remote, final Authenticator auth
+    ) {
+        this(clients, remote, auth, Cache.NOP);
+    }
+
+    /**
+     * New Composer proxy slice with cache.
+     * @param clients HTTP clients
+     * @param remote Remote URI
+     * @param auth Authenticator
+     * @param cache Repository cache
+     * @checkstyle ParameterNumberCheck (6 lines)
+     */
+    public ComposerProxySlice(
+        final ClientSlices clients,
+        final URI remote,
+        final Authenticator auth,
+        final Cache cache
+    ) {
+        super(
+            new SliceRoute(
+                new RtRulePath(
+                    new ByMethodsRule(RqMethod.GET),
+                    new CachedProxySlice(remote(clients, remote, auth), cache)
+                ),
+                new RtRulePath(
+                    RtRule.FALLBACK,
+                    new SliceSimple(new RsWithStatus(RsStatus.METHOD_NOT_ALLOWED))
+                )
+            )
+        );
+    }
+
+    /**
+     * Build client slice for target URI.
+     * @param client Client slices
+     * @param remote Remote URI
+     * @param auth Authenticator
+     * @return Client slice for target URI.
+     */
+    private static Slice remote(
+        final ClientSlices client,
+        final URI remote,
+        final Authenticator auth
+    ) {
+        return new AuthClientSlice(new UriClientSlice(client, remote), auth);
+    }
+}
